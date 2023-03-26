c7316d7b3b4cdedf0d99e2d30f9263bb64f09387
Robert <georobi.016@gmail.com>
2023-03-27 02:10:24 +0300

    Write Readme, Add Database
    -Add Database Structure
    
    - The Readme has more data than this morning, see the updates
    
    - Change test file with some examples, real data for small testing
    
    - Write Readme
    - Add Database Structure
    
    Signed-off-by: Robert <georobi.016@gmail.com>

--- app/db/db.py
+++ app/db/db.py
@@ -2,8 +2,9 @@
 import databases
 import ormar
 import sqlalchemy
-
+import datetime
 from .config import settings
+from typing import Optional
 
 database = databases.Database(settings.db_url)
 metadata = sqlalchemy.MetaData()
@@ -16,11 +17,47 @@ class BaseMeta(ormar.ModelMeta):
 
 class User(ormar.Model):
     class Meta(BaseMeta):
-        tablename = "users"
+        tablename = "Users"
 
+    id: int = ormar.Integer(primary_key=True)                                       # auto-increment automatically set to True
+    email: str = ormar.String(max_length=100, unique=True, nullable=False)
+    password: str = ormar.String(max_length=300, nullable=True, default="")        	# can have this of token_google, but not both
+    token_google: str = ormar.String(max_length=300, nullable=True, default="")     # can have this of password, but not both
+    creted_at: datetime.datetime = ormar.DateTime(default=datetime.datetime.now)	# set to crt datetime each time you call an endpoint
+
+
+class Website(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "Websites"
+    
     id: int = ormar.Integer(primary_key=True)
-    email: str = ormar.String(max_length=128, unique=True, nullable=False)
-    active: bool = ormar.Boolean(default=True, nullable=False)
+    base_url: str = ormar.String(max_length=500, nullable=False, unique=True)
+    name: str = ormar.String(max_length=100, nullable=True)
+    category: str = ormar.String(max_length=300, nullable=False)	# Cat, Cat2, Cat3
+        
+
+class Product(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "Products"
+
+    id: int = ormar.Integer(primary_key = True)
+    website_id: Optional[Website] = ormar.ForeignKey(Website)
+    product_name: str = ormar.String(max_length = 300, nullable=False)
+    category: str = ormar.String(max_length = 300, nullable=True)
+    image: str = ormar.String(max_length = 1000, nullable = True)
+    price: str = ormar.String(max_length = 30, nullable=False)
+    
+
+# ormar has a functionality to automatically create
+# ManyToMany dependencies, we have to check it out
+# in the future
+class CartedProd(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "Carts"
+    
+    entry_id: int = ormar.Integer(primary_key = True)
+    user_id: Optional[User] = ormar.ForeignKey(User)
+    product_id: Optional[Product] = ormar.ForeignKey(Product)
 
 
 engine = sqlalchemy.create_engine(settings.db_url)
--- app/test/test.py
+++ app/test/test.py
@@ -1,6 +1,48 @@
 
-from ..db.db import database, User
+from ..db.db import database, User, Website, Product, CartedProd
 
 async def add_user():
-    await User.objects.get_or_create(email="bogdan@test.com")
-    await User.objects.get_or_create(email="andrei@test.com")
+    ### This is for clean up, DELETES all tables ###
+    # await CartedProd.objects.delete(each=True)
+    # await Product.objects.delete(each=True)
+    # await Website.objects.delete(each=True)
+    # await User.objects.delete(each=True)
+    
+    u1 = await User.objects.get_or_create(email="andrei@test.com", password="andrei")
+    b = await User.objects.get_or_create(email="bogdan@test.com", password="bogdan")
+    ds = await User.objects.get_or_create(email="dragoS@test.com", password="dragoS")
+    da = await User.objects.get_or_create(email="drAgos@test.com", password="drAgos")
+    l = await User.objects.get_or_create(email="lucian@test.com", password="lucian")
+    r = await User.objects.get_or_create(email="robert@test.com", password="robert")
+
+
+    ### websites in here ###
+    emag = await Website.objects.get_or_create(base_url="https://www.emag.ro/search/", name="emag", category="All")
+    pc_garage = await Website.objects.get_or_create(base_url="https://www.pcgarage.ro/", name="pc-garage", category="IT")
+
+    ### products emag here ###
+    p1 = await Product.objects.get_or_create(website_id=emag[0], product_name="Laptop Lenovo", category="IT", 
+                                        image="https://s13emagst.akamaized.net/products/44251/44250473/images/res_2eb9525bba64a13da8e95b10335115f7.jpg",
+                                        price="1.598,17")
+    p2 = await Product.objects.get_or_create(website_id=emag[0], product_name="Laptop Apple", category="IT", 
+                                        image="https://s13emagst.akamaized.net/products/41188/41187834/images/res_4c98933b07a8fd01207cc338b1e2d225.jpg",
+                                        price="9.899,99")
+    p3 = await Product.objects.get_or_create(website_id=emag[0], product_name="Masina de spalat rufe", category="House", 
+                                        image="https://s13emagst.akamaized.net/products/32582/32581589/images/res_7379489e021c4df6b1c82cb9270ddbfd.jpg",
+                                        price="2.579,99")
+
+
+    ## products pc-garage here ###
+    p4 = await Product.objects.get_or_create(website_id=pc_garage[0], product_name="Monitor LED ViewSonic", category="IT", 
+                                        image="https://4.grgs.ro/images/products/1/8561/2541843/full/gaming-xg2431-238-inch-fhd-ips-05-ms-240-hz-hdr-freesync-blur-busters-approved-20-e44ff332e4f3e411d630dd9b34ccd2f1.jpg",
+                                        price="1.499,99")
+    p5 = await Product.objects.get_or_create(website_id=pc_garage[0], product_name="Monitor LED GIGABYTE", category="IT", 
+                                        image="https://2.grgs.ro/images/products/1/2216/2531995/full/gaming-m28u-ae-28-inch-uhd-ips-1-ms-144-hz-usb-c-hdr-freesync-premium-pro-ac94fc1a5d9eb096ec28bc264d821713.jpg",
+                                        price="3.757,98")
+
+    ### add products in users carts here ###
+    await CartedProd.objects.get_or_create(user_id=r[0], product_id=p1[0])
+    await CartedProd.objects.get_or_create(user_id=r[0], product_id=p2[0])
+    await CartedProd.objects.get_or_create(user_id=r[0], product_id=p3[0])
+    await CartedProd.objects.get_or_create(user_id=r[0], product_id=p4[0])
+
